8d41ef063b32d1b3f60de45f441a03627930f525
Martin P <mpala1433@gmail.com>
2019-11-02 11:14:06 -0500

    Updates

---
+++ precache-manifest.2713b669fcea86dc2a3c4415ce36af91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b618345bf50202e516a8bed58edc9af",
+    "url": "/MemGame/index.html"
+  },
+  {
+    "revision": "a100fcc769f84edd4327",
+    "url": "/MemGame/static/css/main.0740bf4c.chunk.css"
+  },
+  {
+    "revision": "80c35f1fecb2aa71ead6",
+    "url": "/MemGame/static/js/2.02230760.chunk.js"
+  },
+  {
+    "revision": "a100fcc769f84edd4327",
+    "url": "/MemGame/static/js/main.7b1c5626.chunk.js"
+  },
+  {
+    "revision": "46a7204709042e1b1cfe",
+    "url": "/MemGame/static/js/runtime-main.b6ce168e.js"
+  }
+]);
